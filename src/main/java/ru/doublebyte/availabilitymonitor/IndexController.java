85c13d106b51390d6538a582f5491ec21324b340
Alexey Nurgaliev <atnurgaliev@gmail.com>
2016-09-01 22:50:18 +0300

    add monitoring form

--- src/main/java/ru/doublebyte/availabilitymonitor/IndexController.java
+++ src/main/java/ru/doublebyte/availabilitymonitor/IndexController.java
@@ -1,15 +1,28 @@
 package ru.doublebyte.availabilitymonitor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.doublebyte.availabilitymonitor.managers.MonitoringManager;
+import ru.doublebyte.availabilitymonitor.types.Monitoring;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     private final MonitoringManager monitoringManager;
 
@@ -29,8 +42,32 @@ public class IndexController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add(Model model) {
+    public String addForm(Model model) {
         return "add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(
+            @RequestParam(name = "url") String url,
+            @RequestParam(name = "check-interval") int checkInterval,
+            @RequestParam(name = "respond-interval") int respondInterval,
+            RedirectAttributes redirectAttributes
+    ) {
+        Monitoring monitoring = new Monitoring(url, checkInterval, respondInterval);
+        List<String> errors = monitoringManager.validate(monitoring);
+
+        if (errors.isEmpty()) {
+            if (monitoringManager.add(monitoring)) {
+                return "redirect:/";
+            } else {
+                errors.add("An error occurred while adding new monitoring");
+            }
+        }
+
+        String error = errors.stream().collect(Collectors.joining(". "));
+        redirectAttributes.addFlashAttribute("error_message", error);
+        redirectAttributes.addFlashAttribute("monitoring", monitoring);
+        return "redirect:/add";
     }
 
 }
--- src/main/java/ru/doublebyte/availabilitymonitor/managers/MonitoringManager.java
+++ src/main/java/ru/doublebyte/availabilitymonitor/managers/MonitoringManager.java
@@ -1,14 +1,19 @@
 package ru.doublebyte.availabilitymonitor.managers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.doublebyte.availabilitymonitor.repositories.MonitoringRepository;
 import ru.doublebyte.availabilitymonitor.types.Monitoring;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Manage monitoring instances
  */
 public class MonitoringManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(MonitoringManager.class);
 
     private MonitoringRepository monitoringRepository;
 
@@ -26,6 +31,48 @@ public class MonitoringManager {
      */
     public List<Monitoring> getAll() {
         return monitoringRepository.findAllByOrderByUrl();
+    }
+
+    /**
+     * Add new monitoring
+     * @param monitoring New monitoring
+     * @return Status of operation
+     */
+    public boolean add(Monitoring monitoring) {
+        try {
+            monitoring = monitoringRepository.save(monitoring);
+            logger.info("Added new monitoring with id {}", monitoring.getId());
+        } catch (Exception e) {
+            logger.error("An error occurred while adding new monitoring", e);
+            return false;
+        }
+
+        //TODO add to scheduler
+
+        return true;
+    }
+
+    /**
+     * Check monitoring for validity
+     * @param monitoring Monitoring to check
+     * @return Check errors
+     */
+    public List<String> validate(Monitoring monitoring) {
+        List<String> errors = new ArrayList<>();
+
+        if (monitoring.getUrl() == null || monitoring.getUrl().isEmpty()) {
+            errors.add("Url should not be empty");
+        }
+
+        if (monitoring.getCheckInterval() <= 0) {
+            errors.add("Check interval should be greater than zero");
+        }
+
+        if (monitoring.getRespondInterval() <= 0) {
+            errors.add("Respond interval should be greater than zero");
+        }
+
+        return errors;
     }
 
 }
